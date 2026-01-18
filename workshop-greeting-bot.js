6782ad27c13d8a0bc28275a6f6de82f443926089
Harman-010 <Harmanpreet0680@gmail.com>
2026-01-18 21:06:00 +0530

    Add files via upload

---
+++ workshop-greeting-bot.js
@@ -0,0 +1,27 @@
+console.log("Hello there!");
+console.log("I am exicited to talk to you");
+let bot 
+bot="teacherBot";
+
+let botLocation="the universe";
+
+console.log("alloe me to introduce myself.");
+let botIntroduction="My name is " + bot + "."; 
+console.log(botIntroduction);
+let botLocationSentence= "I am from " + botLocation +".";
+console.log(botLocationSentence);
+
+bot="professorBot"; 
+let nicknameIntroduction="My nickanme is "+ bot + ".";
+console.log(nicknameIntroduction);
+
+bot ="awesomeTeacherBot";
+
+let newNicknameGreeting="I love my nickname but I wish people would call me  "+ bot +  ".";
+console.log(newNicknameGreeting);
+
+let favoriteSubject=" Computer Science ";
+let favoriteSubjectSentence= " My favorite subject is "+ favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+
+console.log(" Well, it was nice talking to you . Have a good day!");
