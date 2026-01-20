31232a8f60bd2ae7813804e7e6f7ec514ced27c8
Harman-010 <Harmanpreet0680@gmail.com>
2026-01-20 19:49:09 +0530

    Add files via upload

---
+++ Sentencemaker.js
@@ -0,0 +1,19 @@
+let adjective="Happy";
+let noun="Preet";
+let verb="Run";
+let place="Berlin";
+let adjective2="Sad";
+let noun2="Apple";
+
+let firstStory= "Once upon a time, there was a(n) "+ adjective + " " + noun +" who loved to eat "+ noun2 +". The "+ noun +" lived in a "+ place +" and had "+ adjective2 +" nostrils that blew fire when it was "+ verb +".";
+console.log("First story: " +firstStory);
+
+adjective="sad";
+noun="Kid";
+verb="playing";
+place="park";
+adjective2="crying";
+noun2="banana";
+ 
+ let secondStory="Once upon a time, there was a(n) "+ adjective +" "+ noun + " who loved to eat " + noun2 + ". The "+ noun +" lived in a " + place + " and had "+ adjective2+ " nostrils that blew fire when it was " + verb +".";
+ console.log("Second story: "+ secondStory);
---
+++ Triviabot.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName= "Trivia";
+let botLocation= "Desktop";
+let favoriteLanguage="javascript";
+console.log("My name is "+botName +" and I live on "+ botLocation+ ".");
+
+console.log("My favorite programming language is "+ favoriteLanguage+".");
+
+let codingFact= "the " + favoriteLanguage +" is most famous frontend language.";
+console.log(codingFact);
+
+codingFact=favoriteLanguage + " is mostly used in web development.";
+console.log(codingFact);
+
+codingFact="About 98% of the website across the world uses "+ favoriteLanguage +".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+ botName +" from "+ botLocation+".");
