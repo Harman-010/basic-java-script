ef1561ddd4e5d9f6a9f14c4ef65f73988bf1d57d
Preet <Harmanpreet0680@gmail.com>
2026-01-24 16:46:54 +0530

    Add files via upload

---
+++ Logic-checker.js
@@ -0,0 +1,23 @@
+/*let truthyOrFalsy = true;
+truthyOrFalsy = "freeCodeCamp" // will logged true as non-empty strings are truthy values
+truthyOrFalsy = ""
+console.log(Boolean(truthyOrFalsy));*/
+
+
+let hasDeveloperJob = true;
+if(hasDeveloperJob){
+console.log("Timmy is employed as a developer.");
+}
+
+let isTimmyAGamer = false;
+if (isTimmyAGamer){
+  console.log("Timmy loves to play World of Warcraft.");
+}
+
+let timmyAge = 18;
+timmyAge = 15
+if(timmyAge>=16){
+  console.log("Timmy is old enough to drive.");
+} else {
+  console.log("Timmy is not old enough to drive.");
+}
